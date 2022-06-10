d882efe2c38ea7c54d13d19b8f7303fe9a7051bc
Wappen <lenz.koxholt@gmail.com>
2022-06-10 11:49:15 +0200

    Add neural network traversal

--- src/main/java/me/wappen/neural/network/Axon.java
+++ src/main/java/me/wappen/neural/network/Axon.java
@@ -1,11 +1,4 @@
 package me.wappen.neural.network;
 
-public class Axon {
-    Neuron target;
-    double weight;
-
-    public Axon(Neuron target, double weight) {
-        this.target = target;
-        this.weight = weight;
-    }
+public record Axon(Neuron previous, float weight) {
 }
--- src/main/java/me/wappen/neural/network/Network.java
+++ src/main/java/me/wappen/neural/network/Network.java
@@ -4,30 +4,56 @@ import me.wappen.neural.InputProvider;
 import me.wappen.neural.OutputHandler;
 import me.wappen.neural.Processable;
 
-import java.util.List;
+import java.util.*;
 
 public class Network implements Processable {
-    private final Layer[] layers;
+    final List<Neuron> inputs;
+    final List<Neuron> outputs;
 
-    public Network(List<Layer> layers) {
-        this.layers = new Layer[layers.size()];
-        for (int i = 0; i < layers.size(); i++)
-            this.layers[i] = layers.get(i);
+    public Network() {
+        inputs = new LinkedList<>();
+        outputs = new LinkedList<>();
     }
 
-    public void process(InputProvider input, OutputHandler output) {
-        output.handle(process(input.getInput()));
+    public void process(InputProvider provider, OutputHandler handler) {
+        handler.handle(process(provider.getInput()));
     }
 
     @Override
-    public double[] process(double[] inputs) {
-        if (inputs.length != layers[0].size())
-            throw new IllegalArgumentException("inputs' length does not match first layers size");
+    public double[] process(double[] inputValues) {
+        if (inputValues.length != inputs.size())
+            throw new IllegalArgumentException("inputValues' length does not match input layer's size");
 
-        double[] outputs = inputs;
-        for (Layer layer : layers) {
-            outputs = layer.process(outputs);
+        Map<Neuron, Double> traversed = new HashMap<>();
+        {
+            int i = 0;
+            for (Neuron input : inputs)
+                traversed.put(input, inputValues[i++]);
         }
-        return outputs;
+
+        double[] outputValues = new double[outputs.size()];
+        {
+            int i = 0;
+            for (Neuron output : outputs)
+                outputValues[i++] = getValue(traversed, output);
+        }
+        return outputValues;
+    }
+
+    private double getValue(Map<Neuron, Double> cachedValues, Neuron neuron) {
+        double sum = neuron.bias();
+
+        for (Axon inputAxon : neuron.inputAxons()) {
+            if (cachedValues.containsKey(inputAxon.previous())) {
+                sum += cachedValues.get(inputAxon.previous());
+            }
+            else {
+                double val = getValue(cachedValues, inputAxon.previous());
+                cachedValues.put(inputAxon.previous(), val * inputAxon.weight());
+                sum += val;
+            }
+        }
+
+        return neuron.activation().apply(sum);
     }
 }
